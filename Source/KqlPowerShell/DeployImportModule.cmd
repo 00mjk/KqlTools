66b8b923da5e004ef0da5459d517d43bda77141f
REDMOND\naprakas <naprakas@microsoft.com>
2021-04-05 12:07:51 -0700

    updated deploy.cmd to work for actual deployment

---
+++ Source/KqlPowerShell/DeployImportModule.cmd
@@ -0,0 +1,4 @@
+﻿echo -- Deploying and Importing Module --
+mkdir %UserProfile%\Documents\WindowsPowerShell\Modules\KqlPowerShell
+copy lib\* %UserProfile%\Documents\WindowsPowerShell\Modules\KqlPowerShell
+powershell Import-Module %UserProfile%\Documents\WindowsPowerShell\Modules\KqlPowerShell\KqlPowerShell.dll
